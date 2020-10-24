330af3e55f5428e509df9f38650ca5afb1cc3fdc
Thanik Bhongbhibhat <email@thanik.me>
2020-10-24 04:39:06 +0000

    Scaffold session util - always return false for now

--- chitchat/data/session.go
+++ chitchat/data/session.go
@@ -10,3 +10,14 @@ type Session struct {
 	UserID    int
 	CreatedAt time.Time
 }
+
+// Check returns whether the session is valid in the database
+func (s *Session) Check() (valid bool, err error) {
+	var count int
+	q := Db.QueryRow("SELECT count(id) FROM sessions WHERE uuid = $1", s.UUID)
+	err = q.Scan(&count)
+	if count > 0 {
+		valid = true
+	}
+	return
+}
--- chitchat/util.go
+++ chitchat/util.go
@@ -5,10 +5,22 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+
+	"github.com/bhongy/go-web-programming-book/chitchat/data"
 )
 
 func logRequest(req *http.Request) {
 	log.Printf("request: %s\n", req.URL.Path)
+}
+
+// Checks if the session is valid (or still valid)
+func session(req *http.Request) (valid bool, err error) {
+	cookie, err := req.Cookie("_cookie")
+	if err == nil {
+		s := data.Session{UUID: cookie.Value}
+		valid, err = s.Check()
+	}
+	return
 }
 
 func generateHTML(w http.ResponseWriter, data interface{}, filenames []string) {
